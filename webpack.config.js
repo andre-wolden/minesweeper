e8df30d2ffe6edc1db5c42a3d08349cee916f6f4
Andre Wolden <wolden.andre@gmail.com>
2021-02-18 19:48:06 +0100

    update packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const elmMinify = require("elm-minify");
+const {merge} = require('webpack-merge');
 
+const ClosurePlugin = require("closure-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-// to extract the css as a separate file
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+// Production CSS assets - separate, minimised file
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 var MODE =
     process.env.npm_lifecycle_event === "prod" ? "production" : "development";
-var withDebug = !process.env["npm_config_nodebug"];
-console.log('\x1b[36m%s\x1b[0m', `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`);
+var withDebug = !process.env["npm_config_nodebug"] && MODE === "development";
+// this may help for Yarn users
+// var withDebug = !npmParams.includes("--nodebug");
+console.log(
+    "\x1b[36m%s\x1b[0m",
+    `** elm-webpack-starter: mode "${MODE}", withDebug: ${withDebug}\n`
+);
 
 var common = {
     mode: MODE,
@@ -21,7 +27,7 @@ var common = {
         path: path.join(__dirname, "dist"),
         publicPath: "/",
         // FIXME webpack -p automatically adds hash when building for production
-        filename: MODE == "production" ? "[name]-[hash].js" : "index.js"
+        filename: MODE === "production" ? "[name]-[hash].js" : "index.js"
     },
     plugins: [
         new HTMLWebpackPlugin({
@@ -48,20 +54,22 @@ var common = {
                 test: /\.scss$/,
                 exclude: [/elm-stuff/, /node_modules/],
                 // see https://github.com/webpack-contrib/css-loader#url
-                loaders: ["style-loader", "css-loader?url=false", "sass-loader"]
+                use: ["style-loader", "css-loader?url=false", "sass-loader"]
             },
             {
                 test: /\.css$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loaders: ["style-loader", "css-loader?url=false"]
+                use: ["style-loader", "css-loader?url=false"]
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loader: "url-loader",
-                options: {
-                    limit: 10000,
-                    mimetype: "application/font-woff"
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff"
+                    }
                 }
             },
             {
@@ -80,19 +88,17 @@ var common = {
 
 if (MODE === "development") {
     module.exports = merge(common, {
-        plugins: [
-            // Suggested for hot-loading
-            new webpack.NamedModulesPlugin(),
+        optimization: {
             // Prevents compilation errors causing the hot loader to lose state
-            new webpack.NoEmitOnErrorsPlugin()
-        ],
+            noEmitOnErrors: true
+        },
         module: {
             rules: [
                 {
                     test: /\.elm$/,
                     exclude: [/elm-stuff/, /node_modules/],
                     use: [
-                        { loader: "elm-hot-webpack-loader" },
+                        {loader: "elm-hot-webpack-loader"},
                         {
                             loader: "elm-webpack-loader",
                             options: {
@@ -114,31 +120,49 @@ if (MODE === "development") {
             // feel free to delete this section if you don't need anything like this
             before(app) {
                 // on port 3000
-                app.get("/test", function(req, res) {
-                    res.json({ result: "OK" });
+                app.get("/test", function (req, res) {
+                    res.json({result: "OK"});
                 });
             }
         }
     });
 }
+
 if (MODE === "production") {
     module.exports = merge(common, {
+        optimization: {
+            minimizer: [
+                new ClosurePlugin(
+                    {mode: "STANDARD"},
+                    {
+                        // compiler flags here
+                        //
+                        // for debugging help, try these:
+                        //
+                        // formatting: 'PRETTY_PRINT',
+                        // debug: true
+                        // renaming: false
+                    }
+                ),
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        },
         plugins: [
-            // Minify elm code
-            new elmMinify.WebpackPlugin(),
-            // Delete everything from /dist directory and report to user
-            new CleanWebpackPlugin(["dist"], {
+            // Delete everything from output-path (/dist) and report to user
+            new CleanWebpackPlugin({
                 root: __dirname,
                 exclude: [],
                 verbose: true,
                 dry: false
             }),
             // Copy static assets
-            new CopyWebpackPlugin([
-                {
-                    from: "src/assets"
-                }
-            ]),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: "src/assets"
+                    }
+                ]
+            }),
             new MiniCssExtractPlugin({
                 // Options similar to the same options in webpackOptions.output
                 // both options are optional
@@ -160,7 +184,7 @@ if (MODE === "production") {
                 {
                     test: /\.css$/,
                     exclude: [/elm-stuff/, /node_modules/],
-                    loaders: [
+                    use: [
                         MiniCssExtractPlugin.loader,
                         "css-loader?url=false"
                     ]
@@ -168,7 +192,7 @@ if (MODE === "production") {
                 {
                     test: /\.scss$/,
                     exclude: [/elm-stuff/, /node_modules/],
-                    loaders: [
+                    use: [
                         MiniCssExtractPlugin.loader,
                         "css-loader?url=false",
                         "sass-loader"
